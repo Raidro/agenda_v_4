1763cf8e484eba351b5feb810ccbf1189eddf275
Raidro <mateus_sat@hotmail.com>
2019-09-17 20:48:27 -0300

    criando a comunicação com o banco

--- src/main/java/br/senac/rn/agenda/AgendaApplication.java
+++ src/main/java/br/senac/rn/agenda/AgendaApplication.java
@@ -1,13 +1,39 @@
 package br.senac.rn.agenda;
 
+import br.senac.rn.agenda.model.Contato;
+import br.senac.rn.agenda.repository.ContatoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AgendaApplication {
+public class AgendaApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(AgendaApplication.class, args);
+
 	}
 
+	@Autowired
+	ContatoRepository repository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		Contato mateus = new Contato();
+
+		mateus.setNome("Mateus Sátiro");
+		mateus.setFone("19192-2323");
+
+		Contato felipe = new Contato();
+
+		felipe.setFone("Felipe Vasco");
+		felipe.setFone("15158-9595");
+
+		repository.save(mateus);
+		repository.save(felipe);
+
+
+	}
 }
