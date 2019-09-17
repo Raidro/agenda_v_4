8f2e8a104d8174dab6ff3113c1a845d050af16f3
MATEUS PERREIRA SATIRO REYES <a9261456@rn.senac.br>
2019-09-17 19:57:46 -0300

    primeiro commit

---
+++ src/main/java/br/senac/rn/agendav4/AgendaV4Application.java
@@ -0,0 +1,13 @@
+package br.senac.rn.agendav4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaV4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaV4Application.class, args);
+	}
+
+}
