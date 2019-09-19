3ffed228279ef67f86842b2790d3ffe93d547149
Raidro <mateus_sat@hotmail.com>
2019-09-18 21:13:37 -0300

    trasformando em um controller

--- src/main/java/br/senac/rn/agenda/controller/ContatoController.java
+++ src/main/java/br/senac/rn/agenda/controller/ContatoController.java
@@ -3,11 +3,14 @@ package br.senac.rn.agenda.controller;
 import br.senac.rn.agenda.model.Contato;
 import br.senac.rn.agenda.services.ContatoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
+@Controller
 public class ContatoController {
 
 
@@ -19,7 +22,7 @@ public class ContatoController {
         return "index";
     }
 
-    @GetMapping("/ contatos")
+    @GetMapping("/contatos")
     public String listaTodos(Model model){
         List<Contato>contatos = service.listaTodos();
         model.addAttribute("contatos",contatos);
