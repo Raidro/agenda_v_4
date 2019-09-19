614b18b9327f9457189846522374de1227b51b2b
Raidro <mateus_sat@hotmail.com>
2019-09-18 21:06:47 -0300

    criando a pagina de contatos e trazendo do banco pro front

--- src/main/java/br/senac/rn/agenda/controller/ContatoController.java
+++ src/main/java/br/senac/rn/agenda/controller/ContatoController.java
@@ -1,8 +1,12 @@
 package br.senac.rn.agenda.controller;
 
+import br.senac.rn.agenda.model.Contato;
 import br.senac.rn.agenda.services.ContatoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 public class ContatoController {
 
@@ -10,9 +14,16 @@ public class ContatoController {
     @Autowired
     private ContatoService service;
 
-    @GetMapping("/contato")
+    @GetMapping("/ ")
     public String helloWorld(){
         return "index";
+    }
+
+    @GetMapping("/ contatos")
+    public String listaTodos(Model model){
+        List<Contato>contatos = service.listaTodos();
+        model.addAttribute("contatos",contatos);
+        return "contatos";
     }
 
 
