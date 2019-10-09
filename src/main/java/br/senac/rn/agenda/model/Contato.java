fa063f891840c0508a0699f6b5601290d87c9afc
Raidro <mateus_sat@hotmail.com>
2019-10-09 19:15:07 -0300

    criado os applications separados

--- src/main/java/br/senac/rn/agenda/model/Contato.java
+++ src/main/java/br/senac/rn/agenda/model/Contato.java
@@ -1,16 +1,18 @@
 package br.senac.rn.agenda.model;
 
+import lombok.Data;
+
 import javax.persistence.*;
-import java.util.Objects;
+
 
 @Entity
 @Table(name = "tb_contatos")
+@Data
 public class Contato {
 
     @Id
     @Column(name = "con_id")
-    @SequenceGenerator(name = "tb_contatos_id_sequence", sequenceName = "tb_contatos_id_sequence", allocationSize = 1)
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "tb_contatos_id_sequences")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "con_nome", nullable = false)
@@ -19,40 +21,4 @@ public class Contato {
     @Column(name = "con_fone", nullable = false)
     private String fone;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getFone() {
-        return fone;
-    }
-
-    public void setFone(String fone) {
-        this.fone = fone;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Contato contato = (Contato) o;
-        return id.equals(contato.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
 }
