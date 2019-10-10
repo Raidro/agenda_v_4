6edb3e63403db96319dc726c449725337e0034f0
Raidro <mateus_sat@hotmail.com>
2019-10-10 19:46:04 -0300

    adcionado o config do cors

---
+++ src/main/java/br/senac/rn/agenda/config/AgendaApplicationConfig.java
@@ -0,0 +1,14 @@
+package br.senac.rn.agenda.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class AgendaApplicationConfig  implements WebMvcConfigurer {
+
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("GET", "POST");
+    }
+
+}
